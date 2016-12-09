e2bc8c7d4c35301bff9c27553d5c2c5a37f5df6c
ukitaka <ukitaka@linecorp.com>
2016-12-09 18:46:46 +0900

    Remove Package.swift

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "TryParsec",
-    dependencies: [
-        .Package(url: "https://github.com/inamiy/Runes.git", "4.0.0-inamiy.1"),
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
-    ]
-)
